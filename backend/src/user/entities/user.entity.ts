d5f1ce0b720ccc1e0ffa045d2a9c4f7fc90bf286
MrM0lten <jean.isserstedt@gmx.de>
2023-10-04 13:22:31 +0100

    init branch, started adding on UserEntity

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -5,6 +5,24 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ nullable: true })
+  intraID: number;
+
   @Column({ nullable: false })
   name: string;
+
+  @Column({ nullable: true })
+  avatar: string;
+
+  @Column('decimal',{ nullable: false, precision: 6, scale: 2})
+  level: number;
+
+  @Column({ nullable: false })
+  matches: number;
+
+  @Column({ nullable: false })
+  wins: number;
+
+  //RelationList
+
 }
