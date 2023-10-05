021ab92acc521942391313e225ad9c7e96620bd2
Cosmo <htam@student.42berlin.de>
2023-10-05 20:04:39 +0200

    chat in JSON format

---
+++ chatty/src/chat/Ichat.ts
@@ -0,0 +1,6 @@
+export interface Chat {
+  id: number;
+  userName: string;
+  room: string;
+  message: string;
+}
--- chatty/src/chat/chat_gateway.ts
+++ chatty/src/chat/chat_gateway.ts
@@ -1,29 +1,66 @@
 import { OnModuleInit } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server } from 'socket.io'
+import { Namespace, Server, Socket } from 'socket.io'
+import { Chat } from "./Ichat";
 
 @WebSocketGateway({
   cors: {
-    origins: 'http://10.15.203.3:3000'
-  }
+    origins: 'http://10.15.202.3:3000'
+  },
+  namespace : 'chat'
 })
 export class ChatGateway implements OnModuleInit {
 
   @WebSocketServer()
-  server: Server;
+  server: Namespace;
 
   onModuleInit() {
-    this.server.on('connection', (socket) => {
-      console.log(socket.id);
+    this.server.on('connection', (client) => {
+      console.log(client.id);
       console.log('Connected');
     });
   }
 
   @SubscribeMessage('newMessage')
-  handle(@MessageBody() body: any) {
+  readAndSend(@MessageBody() body: any) {
+    console.log('test');
+    // this.server.on('newMessage', (body) => {
+    //   console.log('test');
+    //   const data = JSON.parse(body);
+    //   console.log(data.room);
+    //   console.log(data.message);
+    //   // this.server.emit('chatMessage', body);
+    //   this.server.to(data.room).emit(data.message);
+    // })
     console.log(body);
-    this.server.emit('chatMessage', body);
+    const data = JSON.parse(JSON.stringify(body));
+    console.log(data['room']);
+    console.log(data['message']);
+    this.server.to(data.room).emit('chatMessage', data.message);
+
+    // console.log(body);
+    // this.server.emit('chatMessage', body);
+    
   }
+
+  @SubscribeMessage('joinRoom')
+  joinChannel(client: Socket, room: string) {
+    client.join(room);
+    console.log(`${client.id} joined the room: ${room}`);
+
+    this.server.to(room).emit('message', `${client.id} joined the room: ${room}`);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  leaveChannel(client: Socket, room: string) {
+    client.leave(room);
+
+    console.log(`${client.id} left the room: ${room}`);
+    this.server.to(room).emit('message', `${client.id} left the room: ${room}`);
+
+  }
+
+
 
 }
 
