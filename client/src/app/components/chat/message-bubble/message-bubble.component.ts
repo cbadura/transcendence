ef7be9bffccc8d939a2956fb49c39c033bfb1a61
Luca Fischer <lucafischer@fastmail.com>
2023-10-06 15:56:14 +0200

    Add chat interface

---
+++ client/src/app/components/chat/message-bubble/message-bubble.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tcd-message-bubble',
+  templateUrl: './message-bubble.component.html',
+  styleUrls: ['./message-bubble.component.css']
+})
+export class MessageBubbleComponent {
+
+}
---
+++ client/src/app/components/chat/message-info/message-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tcd-message-info',
+  templateUrl: './message-info.component.html',
+  styleUrls: ['./message-info.component.css']
+})
+export class MessageInfoComponent {
+
+}
