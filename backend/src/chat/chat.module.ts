ab5a793ee8e35ac06ed28562e595eed627679fb3
Cosmo <htam@student.42berlin.de>
2023-10-09 19:57:04 +0200

    reset chat

--- backend/src/chat/chat.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common"
-import { ChatGateway } from "./chat.gateway";
-import { ChatService } from './chat.service';
-
-@Module({
-  providers: [ChatGateway, ChatService],
-})
-export class ChatModule {}
