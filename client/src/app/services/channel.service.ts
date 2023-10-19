1c7dc445c47f9536626d1e1a727f09b30c8daa97
Esperanza <wildfrenzy0432@gmail.com>
2023-10-19 18:04:26 +0200

    added event listeners: ban, mute, kick, invite, leave, add/remove admin

--- client/src/app/services/channel.service.ts
+++ client/src/app/services/channel.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, Inject } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
-import { BehaviorSubject, Subject, Observable, map } from 'rxjs';
+import {Inject, Injectable} from '@angular/core';
+import {Socket} from 'ngx-socket-io';
+import {BehaviorSubject, Subject} from 'rxjs';
 
-import { Channel } from '../shared/chat/Channel';
-import { EUserRole } from '../shared/macros/EUserRole';
-import { ESocketMessage } from '../shared/chat/ESocketMessage';
+import {Channel} from '../shared/chat/Channel';
+import {EUserRole} from '../shared/macros/EUserRole';
+import {ESocketMessage} from '../shared/chat/ESocketMessage';
 
 @Injectable({
   providedIn: 'root'
@@ -32,7 +32,7 @@ export class ChannelService {
     let newChannel = {
       channelName: channel.name,
       mode: channel.mode,
-      password: '' 
+      password: ''
     }
     if (password) {
       newChannel.password = password;
@@ -45,7 +45,7 @@ export class ChannelService {
     let newChannel = {
       channelName: channel.name,
       currName: channel.name
-      // password: '' 
+      // password: ''
     }
     /* if (password) {
       newChannel.password = password;
@@ -59,7 +59,7 @@ export class ChannelService {
       currName: currName,
       channelName: channel.name,
       mode: channel.mode,
-      password: '' 
+      password: ''
     }
     if (password) {
       newChannel.password = password;
@@ -76,9 +76,77 @@ export class ChannelService {
     this.chatSocket.emit(ESocketMessage.TRY_DELETE_CHANNEL, ch);
   }
 
-  // mute
-    // kick
-    // ban
+  /*~~~~~Nadiia~~~~~*/
+
+  // expirationTimestamp: unix timestamp in seconds;
+  banUser(chName: string, targetId: number, timestamp: number) {
+    let ban = {
+      channelName: chName,
+      targetUserId: targetId,
+      expirationTimestamp: timestamp
+    }
+    console.log('BAN', ban);
+    this.chatSocket.emit(ESocketMessage.TRY_BAN_FROM_CHANNEL, ban);
+  }
+
+  muteUser(chName: string, targetId: number, timestamp: number) {
+    let mute = {
+      channelName: chName,
+      targetUserId: targetId,
+      expirationTimestamp: timestamp
+    }
+    console.log('MUTE', mute);
+    this.chatSocket.emit(ESocketMessage.TRY_MUTE_FROM_CHANNEL, mute);
+  }
+
+  kickUser(chName: string, targetId: number) {
+    let kick = {
+      channelName: chName,
+      targetUserId: targetId
+    }
+    console.log('KICK', kick);
+    this.chatSocket.emit(ESocketMessage.TRY_KICK_FROM_CHANNEL, kick);
+  }
+
+  inviteUser(chName: string, targetId: number) {
+    let invite = {
+      channelName: chName,
+      targetUserId: targetId
+    }
+    console.log('INVITE', invite);
+    this.chatSocket.emit(ESocketMessage.TRY_INVITE_TO_CHANNEL, invite);
+  }
+
+  leaveChannel(chName: string) {
+    //TODO add info if owner is leaving :
+    //  option: EChannelLeaveOption;
+    //  transferId: number;
+    let leave = {
+      channelName: chName
+    }
+    console.log('LEAVE', leave);
+    this.chatSocket.emit(ESocketMessage.TRY_LEAVE_CHANNEL, leave);
+  }
+
+  addAdmin(chName: string, targetId: number) {
+    let addAdmin = {
+      channelName: chName,
+      userId: targetId
+    }
+    console.log('ADD ADMIN', addAdmin);
+    this.chatSocket.emit(ESocketMessage.TRY_ADD_ADMIN, addAdmin);
+  }
+
+  removeAdmin(chName: string, targetId: number) {
+    let removeAdmin = {
+      channelName: chName,
+      userId: targetId
+    }
+    console.log('REMOVE ADMIN', removeAdmin);
+    this.chatSocket.emit(ESocketMessage.TRY_REMOVE_ADMIN, removeAdmin);
+  }
+
+  /*~~~~~~~~~~~~~~~~*/
 
   subscribeToEvents() {
     this.chatSocket?.on(
@@ -104,7 +172,7 @@ export class ChannelService {
           role: EUserRole.OWNER,
           isBanned: false,
           isMuted: false,
-          usersIds: [1] 
+          usersIds: [1]
         }
         this.channels.push(channel);
         this.serverChannels.next(this.channels);
@@ -135,8 +203,138 @@ export class ChannelService {
         console.log('AFTER DELETED', this.channels);
         this.serverChannels.next(this.channels);
     });
+
+    /*~~~~~Nadiia~~~~~*/
+
+    this.chatSocket?.on(
+      ESocketMessage.JOINED_TO_CHANNEL,
+      (data: any) => {
+        console.log('JOINED', data);
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+          {
+            ch.usersIds = data.channelUsersIds;
+            ch.role = EUserRole.USER;
+          }
+        });
+        this.serverChannels.next(this.channels);
+      });
+
+    this.chatSocket?.on(
+      ESocketMessage.BANNED_FROM_CHANNEL,
+      (data: any) => {
+        console.log('BANNED', data);
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+          {
+            /* uncomment this part, when client will know it's id
+            if (data.targetUserId === myID){
+              ch.isBanned = true;
+              ch.banExpTime = data.expirationTimestamp;
+            }*/
+            ch.usersIds = ch.usersIds.filter((id) => id !== data.targetUserId);
+            /* uncomment this when you add channel admins
+            if (ch.role === EUserRole.OWNER)
+              ch.adminIds = ch.adminIds.filter((id) => id !== data.targetUserId);
+            */
+          }
+        });
+        this.serverChannels.next(this.channels);
+      });
+
+    this.chatSocket?.on(
+      ESocketMessage.MUTED_FROM_CHANNEL,
+      (data: any) => {
+        console.log('MUTED', data);
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+          {
+              ch.isMuted = true;
+              ch.muteExpTime = data.expirationTimestamp;
+          }
+        });
+        this.serverChannels.next(this.channels);
+      });
+
+    this.chatSocket?.on(
+      ESocketMessage.KICKED_FROM_CHANNEL,
+      (data: any) => {
+        console.log('KICKED', data);
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+            ch.usersIds = ch.usersIds.filter((id) => id !== data.targetUserId);
+          /* uncomment this when you add channel admins
+           if (ch.role === EUserRole.OWNER)
+             ch.adminIds = ch.adminIds.filter((id) => id !== data.targetUserId);
+           */
+        });
+        this.serverChannels.next(this.channels);
+      });
+
+    this.chatSocket?.on(
+      ESocketMessage.INVITED_TO_CHANNEL,
+      (data: any) => {
+        console.log('INVITED', data);
+        /* uncomment when handling invites implemented
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+            // here add to invitelist ^
+        });*/
+        this.serverChannels.next(this.channels);
+      });
+
+    this.chatSocket?.on(
+      ESocketMessage.LEFT_CHANNEL,
+      (data: any) => {
+        console.log('LEFT', data);
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+          {
+            ch.usersIds = ch.usersIds.filter((id) => id !== data.targetUserId);
+            /* uncomment this when you add channel admins
+           if (ch.role === EUserRole.OWNER)
+             ch.adminIds = ch.adminIds.filter((id) => id !== data.targetUserId);
+           */
+            /* uncomment this part, when client will know it's id
+              if (data.transferId === myID)
+                 ch.role = EUserRole.OWNER;
+             */
+         }
+       });
+       this.serverChannels.next(this.channels);
+     });
+
+    this.chatSocket?.on(
+      ESocketMessage.ADDED_ADMIN,
+      (data: any) => {
+        console.log('ADDED ADMIN', data);
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+          {
+            /* uncomment this when you add channel admins
+               ch.adminIds.push(data.userId);
+            */
+          }
+        });
+        this.serverChannels.next(this.channels);
+      });
+
+    this.chatSocket?.on(
+      ESocketMessage.REMOVED_ADMIN,
+      (data: any) => {
+        console.log('REMOVED ADMIN', data);
+        this.channels.find((ch) => {
+          if (ch.name === data.channelName)
+          {
+            /* uncomment this when you add channel admins
+             ch.adminIds = ch.adminIds.filter((id) => id !== data.targetUserId);6
+           */
+          }
+        });
+        this.serverChannels.next(this.channels);
+      });
+
+   /*~~~~~~~~~~~~~~~~*/
   }
 
-    // add admin
-    // remove admin
 }
