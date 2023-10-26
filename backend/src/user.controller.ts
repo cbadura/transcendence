757aabc2ef7b81cc9cb0dc00687260512da9b35a
lucafisc <lucafischer@fastmail.com>
2023-10-26 15:48:48 +0200

    Fix image upload

---
+++ backend/src/user.controller.ts
@@ -0,0 +1,119 @@
+import { ParseIntPipe,Body, Controller, Get,Res, Post, Query,Param,NotFoundException,Put, Delete, UseInterceptors, UploadedFile, Req, BadRequestException } from '@nestjs/common';
+import {FileInterceptor} from '@nestjs/platform-express'
+import { UserService } from './user/user.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './user/dto/create-user.dto';
+import { UpdateUserDto } from './user/dto/update-user.dto';
+import { Express } from 'express'
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import {Response} from 'express'
+import * as path from 'path';
+import { Request } from 'express';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  GetUsers(){
+    return this.userService.getUsers();
+  }
+
+  @Get('dummy')
+  createDummyUsers(){
+    this.userService.createDummyUsers();
+  }
+
+  @Delete('dummy')
+  deleteUserDatabase(){
+    this.userService.deleteUserDatabase();
+  }
+
+  //todo: prevent uploading files if user doesnt exist 
+  @Post(':id/profilepic')
+  @UseInterceptors(FileInterceptor('file',{
+    storage: diskStorage({
+      destination: './uploadedData/profilepictures',
+      filename: (req,file,callback) => {
+          const userId = req.params.id;
+          // const uniqueSuffix = Date.now() 
+          // console.log(req);
+          const extension = extname(file.originalname)
+          const filename =`profilepic_user_${userId}${extension}`;
+          callback(null,filename);
+      }
+    })
+  }))
+  uploadProfilePicture(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id',ParseIntPipe) id: number,
+    @Req() request: Request,
+    ) {
+
+    // const baseUrl = request.protocol + '://' + request.get('host');
+    const userProfileImageURL = `http://localhost:3000/users/profilepic/${file.filename}`
+
+    let updateDTO = new UpdateUserDto();
+    console.log('successfully uploaded file to: ',userProfileImageURL);
+    updateDTO.avatar = userProfileImageURL;
+    try {
+       this.userService.updateUser(id,updateDTO);
+    } catch (error) {
+        throw new NotFoundException()
+    }
+      return {img: userProfileImageURL};
+  }
+
+  // this makes sense, but blocks the other
+  @Get('profilepic/:filename')
+  ServeUploadedFile(@Param('filename')filename:string, @Res() res: Response){
+      const filePath = path.join(__dirname, '../../', 'uploadedData/profilepictures/', filename);
+      res.sendFile(filePath)
+  }
+
+
+  // GET /user/:id --> {...} get a single ninja
+  @Get(':id')
+  getUser(@Param('id',ParseIntPipe) id: number) {
+      try {
+          return this.userService.getUser(id);
+      } catch (error) {
+          throw new NotFoundException()
+      }
+  };
+
+  @Put(':id')
+  updateUser(@Param('id',ParseIntPipe ) id: number,@Body() dto: UpdateUserDto) {
+      try {
+          return this.userService.updateUser(id,dto);
+      } catch (error) {
+          throw new NotFoundException()
+      }
+  };
+
+  @Delete(':id')
+  deleteUser(@Param('id',ParseIntPipe) id: number){
+    return this.userService.deleteUser(id);
+  }
+
+  @Get(':id/matches')
+  getUserMatches(@Param('id',ParseIntPipe) id: number){
+    return this.userService.getUserMatches(id);
+  }
+
+  @Get(':id/relationship')
+  getUserRelationships(@Param('id',ParseIntPipe) id: number,
+  @Query('filter') filter?: string,
+  ){
+      if (filter && filter !== 'friend' && filter !== 'blocked') 
+        throw new BadRequestException('filterField must be either "friend" or "blocked"');
+    return this.userService.getUserRelationships(id,filter);
+  }
+
+  @Post()
+  createUser(@Body() dto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(dto);
+  }
+
+}
