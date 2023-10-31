94b10f651f9bee34bc79e2d091b6b5c3b0525988
cbadura <c_badura@web.de>
2023-10-31 11:44:04 +0100

    delete joinQueue

--- client/src/app/components/game/game.component.ts
+++ client/src/app/components/game/game.component.ts
@@ -71,7 +71,6 @@ export class GameComponent implements CanComponentDeactivate {
         console.log('START FROM INVITE');
         this.gameType = params['gameType'];
         this.status = 'waiting';
-        this.gameService.JoinQueue(this.myUser.id, this.gameType);
         this.subscribeToEventObject();
       }
     });
