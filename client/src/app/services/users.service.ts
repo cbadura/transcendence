1bc869d0c9807bec1bf6b79ceed215779c212665
lucafisc <lucafischer@fastmail.com>
2023-11-06 09:40:57 +0100

    Add listener to STATUS_UPDATE

--- client/src/app/services/users.service.ts
+++ client/src/app/services/users.service.ts
@@ -1,22 +1,42 @@
 // user.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, Subject, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from 'src/app/shared/interfaces/user';
+import { UserDataService } from './user-data.service';
+import { Socket } from 'ngx-socket-io';
+import { EUserStatus } from '../shared/macros/EUserStatus';
+import { EUserMessages } from '../shared/macros/EUserMessages';
+
+interface UserStatus {
+  id: number;
+  status: EUserStatus;
+}
 
 interface UserRelationship {
-  id: number;
-  primary_user_id: number;
-  relational_user_id: number;
-  relationship_status: string;
-}
+	id: number;
+	primary_user_id: number;
+	relational_user_id: number;
+	relationship_status: string;
+  }
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  userSocket: Socket | null = null;
+  statuses: UserStatus[] = [];
+  statusSubject = new BehaviorSubject<UserStatus[]>(this.statuses);
+  statusChatObs$ = this.statusSubject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private userService: UserDataService,
+  ) {
+    this.userSocket = userService.userSocket;
+    this.subscribeToEvents();
+  }
 
   getUsers(): Observable<User[]> {
     const usersUrl = 'http://localhost:3000/users';
@@ -30,9 +50,16 @@ export class UserService {
     return this.http.get<UserRelationship[]>(requestUrl);
   }
 
-  getMatches(id: number) : Observable<any[]> {
-	const matchesUrl = `http://localhost:3000/users/${id}/matches`;
-	return this.http.get<any[]>(matchesUrl)
+  getMatches(id: number): Observable<any[]> {
+    const matchesUrl = `http://localhost:3000/users/${id}/matches`;
+    return this.http.get<any[]>(matchesUrl);
   }
 
+  subscribeToEvents() {
+    this.userSocket?.on(EUserMessages.STATUS_UPDATE, (data: any) => {
+      console.log(EUserMessages.STATUS_UPDATE, data);
+    //   this.statuses
+      this.statusSubject.next(this.statuses);
+    });
+  }
 }
