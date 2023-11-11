f30b09c19b1417147f0f7ae7bde37e362675b590
MrM0lten <jean.isserstedt@gmx.de>
2023-11-11 16:37:39 +0100

    implemented viewing profile from chat message info component

--- client/src/app/components/chat/message-info/message-info.component.ts
+++ client/src/app/components/chat/message-info/message-info.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Post } from 'src/app/shared/interfaces/post';
 
 @Component({
@@ -6,6 +8,30 @@ import { Post } from 'src/app/shared/interfaces/post';
   templateUrl: './message-info.component.html',
 })
 export class MessageInfoComponent {
+  private serverAddress: string = 'http://localhost:3000';
+  constructor(private http: HttpClient,private router: Router) {}
+  ngOnInit(): void {}
+
 @Input() post!: Post;
 @Input() isMe!: boolean;
+
+clickOnCard = (event: Event) => {
+  if(this.isMe){ //right now do nothing, because you dont want to accidentally move to own profile
+    // event.stopPropagation();
+    // this.router.navigate(['profile',]); 
+  }
+  else{
+    this.http.get(this.serverAddress + '/users/' + this.post.senderId, { withCredentials: true }).subscribe(
+      (data) => {
+        console.log('EDIT', JSON.stringify(data));
+        console.log('data =',data)
+        event.stopPropagation();
+        this.router.navigate(['profile', 'profile', data]); 
+        
+      },
+      (error) => {
+        window.alert('Error editing user: ' + JSON.stringify(error));
+      })
+  };
+  }
 }
